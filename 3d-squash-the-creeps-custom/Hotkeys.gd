c2233a2a8aa418cdbd8e2756a573fd959ebaa436
Telmo "Trooper <telmo.trooper@gmail.com>
2021-06-01 23:43:22 -0300

    Close game on ESC

--- 3d-squash-the-creeps-custom/Hotkeys.gd
+++ 3d-squash-the-creeps-custom/Hotkeys.gd
@@ -4,6 +4,9 @@ func _process(_delta):
   if Input.is_action_just_pressed("ui_toggle_fullscreen"):
     OS.window_fullscreen = !OS.window_fullscreen
 
+  if Input.is_action_just_released("ui_close_game"):
+    get_tree().quit()
+
   if Input.is_action_just_pressed("ui_fast_forward"):
     Engine.time_scale = 2.25
   elif Input.is_action_just_released("ui_fast_forward"):
