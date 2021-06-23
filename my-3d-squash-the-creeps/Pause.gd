9f35696063061e3872c443365ef32492f65eee24
Telmo "Trooper <telmo.trooper@gmail.com>
2021-06-23 20:35:59 -0300

    Added basic menu on pause

--- my-3d-squash-the-creeps/Pause.gd
+++ my-3d-squash-the-creeps/Pause.gd
@@ -1,6 +1,20 @@
 extends Control
 
+func pause():
+  get_tree().paused = not get_tree().paused
+  visible = get_tree().paused
+  if visible:
+    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+  else:
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _input(event):
   if event.is_action_pressed("pause"):
-    get_tree().paused = not get_tree().paused
-    visible = get_tree().paused
+    pause()
+
+func _on_ResumeButton_pressed():
+  pause()
+
+
+func _on_ExitButton_pressed():
+  get_tree().quit()
