d514c2203bcf6dfbc06a688a9c57f6da2d044c23
Telmo "Trooper <telmo.trooper@gmail.com>
2021-07-03 16:07:52 -0300

    Adjusted configuration to fill missing fields

--- Configuration.gd
+++ Configuration.gd
@@ -7,12 +7,22 @@ func _ready():
   var load_file = config.load(file_path)
   
   if load_file != OK:
-    config.set_value("audio", "music_volume", 100)
-    config.set_value("audio", "sound_volume", 100)
-    config.set_value("controls", "mouse_sensitivity", 0.5)
-    config.set_value("display", "fullscreen", false)
     config.save(file_path)
   
+  # Set default values if missing
+  if not config.has_section_key("audio", "music_volume"):
+    config.set_value("audio", "music_volume", 100)
+  
+  if not config.has_section_key("audio", "sound_volume"):
+    config.set_value("audio", "sound_volume", 100)
+  
+  if not config.has_section_key("controls", "mouse_sensitivity"):
+    config.set_value("controls", "mouse_sensitivity", 0.5)
+  
+  if not config.has_section_key("display", "fullscreen"):
+    config.set_value("display", "fullscreen", false)
+  
+  # Handle fullscreen
   if config.get_value("display", "fullscreen") == true:
     OS.window_fullscreen = true
 
