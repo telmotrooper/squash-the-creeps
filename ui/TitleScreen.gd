f52340e5bd57ca80881e58dd2d08d91edb398b68
Telmo "Trooper <telmo.trooper@gmail.com>
2021-08-11 23:04:03 -0300

    Complete rotation

--- ui/TitleScreen.gd
+++ ui/TitleScreen.gd
@@ -2,8 +2,6 @@ extends Node
 
 func _ready():
   $Spatial/AnimationPlayer.play("rotating_skybox")
-  $Spatial/AnimationPlayer.playback_speed = 0.2
-  
 
 func _on_NewGameButton_pressed():
   get_tree().change_scene("res://Main.tscn")
