0a82795e73a6263ca7b608333ce24d0bf53588ed
Telmo "Trooper <telmo.trooper@gmail.com>
2021-08-11 21:06:47 -0300

    Space moving

--- ui/TitleScreen.gd
+++ ui/TitleScreen.gd
@@ -1,5 +1,10 @@
 extends Node
 
+func _ready():
+  $Spatial/AnimationPlayer.play("rotating_skybox")
+  $Spatial/AnimationPlayer.playback_speed = 0.2
+  
+
 func _on_NewGameButton_pressed():
   get_tree().change_scene("res://Main.tscn")
 
