ebf906d6a7ce0632d82e213386f5e85189f29a2d
Telmo "Trooper <telmo.trooper@gmail.com>
2021-08-12 21:57:07 -0300

    When 'New Game' is clicked, fade out menu and load first stage

--- ui/TitleScreen.gd
+++ ui/TitleScreen.gd
@@ -1,11 +1,16 @@
 extends Node
 
+var main_scene = load("res://Main.tscn")
+
 func _ready():
   $Spatial/AnimationPlayerSkybox.play("rotating_skybox")
   $Spatial/AnimationPlayerSpaceship.play("flying_in_space")
 
 func _on_NewGameButton_pressed():
-  get_tree().change_scene("res://Main.tscn")
+  $AnimationPlayerMenu.play("menu_fade_out")
 
 func _on_ExitButton_pressed():
   get_tree().quit()
+
+func _on_AnimationPlayerMenu_animation_finished(anim_name):
+  get_tree().change_scene("res://Main.tscn")
