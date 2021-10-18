6ca2d110a4778b4fdbee95d7a0f91a75d5a90cb2
Telmo "Trooper <telmo.trooper@gmail.com>
2021-10-18 19:56:27 -0300

    Renamed 'Main' scene to 'TestMap'; Working on implementing basic AI.

---
+++ enemy/BlueEnemy.gd
@@ -0,0 +1,26 @@
+extends Enemy
+
+onready var raycast = $RayCast
+
+enum {
+  IDLE,
+  PATROLLING,
+  ALERT
+}
+
+var state = IDLE
+
+func _ready():
+  pass
+
+func _process(delta):
+  if raycast.is_colliding():
+    state = ALERT
+  
+  match state:
+    IDLE:
+      print("IDLE")
+    PATROLLING:
+      print("PATROLLING")
+    ALERT:
+      print("ALERT")
