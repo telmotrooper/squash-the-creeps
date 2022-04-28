87a3fb13da8c5262105ff9636e6ea42188d6825d
Telmo "Trooper <telmo.trooper@gmail.com>
2022-04-28 10:58:57 -0300

    Develop (#27)
    * Moved 'Boundary.gd' to 'maps' and 'Pause.gd' to 'ui'
    
    * Renaming files to follow suggested style guide
    
    * Renamed fodler from 'grass' to 'grass_52'
    
    * Renaming files to follow suggested style guide #2
    
    * Renaming files to follow suggested style guide #3
    
    * Renaming files to follow suggested style guide #4
    
    * Renaming files to follow suggested style guide #5
    
    * Renamed font files
    
    * Renaming files to follow suggested style guide #6
    
    * Renaming files to follow suggested style guide #7
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

---
+++ enemy/enemy.gd
@@ -0,0 +1,46 @@
+extends KinematicBody
+class_name Enemy
+
+signal squashed
+
+export var min_speed := 10.0
+export var max_speed := 18.0
+
+var velocity = Vector3.ZERO
+
+func _ready():
+  self.connect("squashed", GameState.UserInterface.get_node("ScoreLabel"), "_on_Enemy_squashed")
+
+func _physics_process(_delta):
+  velocity = move_and_slide(velocity, Vector3.UP)
+
+func initiliaze(start_position, player_position, rotate = true, speed = null):
+  translation = start_position
+  look_at(player_position, Vector3.UP)
+  # Ignore height of player position, spawn looking straight.
+  rotation.x = 0
+  
+  # Rotate between -45 degrees and 45 degrees.
+  if rotate:
+    rotate_y(rand_range(-PI / 4.0, PI / 4.0))
+  
+  if speed == null:
+    speed = rand_range(min_speed, max_speed)
+  
+  velocity = Vector3.FORWARD * speed
+  velocity = velocity.rotated(Vector3.UP, rotation.y)
+  $AnimationPlayer.playback_speed = speed / min_speed
+
+func squash():
+  emit_signal("squashed")
+  velocity = Vector3.ZERO
+  $CollisionShape.disabled = true
+  $AnimationPlayer.playback_speed = 1
+  $AnimationPlayer.play("squash")
+  GameState.Audio.play("res://art/slime_jump.ogg")
+
+func _on_VisibilityNotifier_screen_exited():
+  queue_free()
+
+func kill():
+  queue_free()
---
+++ ui/title_screen.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var main_scene: PackedScene = load("res://maps/test_map.tscn")
+const MENU_FADE_OUT = "menu_fade_out"
+
+func _on_NewGameButton_pressed():
+  $AnimationPlayerMenu.play(MENU_FADE_OUT)
+
+func _on_ExitButton_pressed():
+  get_tree().quit()
+
+func _on_AnimationPlayerMenu_animation_finished(anim_name):
+  if anim_name == MENU_FADE_OUT:
+    GameState.change_map("test_map")
