c3cdc1603ee763f6a9d4216eed2c984839f28630
Telmo Valverde <telmo.valverde@tarmac.io>
2022-04-27 23:38:07 -0300

    Check if animation is 'menu_fade_out' before changing maps

--- ui/TitleScreen.gd
+++ ui/TitleScreen.gd
@@ -1,12 +1,14 @@
 extends Node
 
 var main_scene = load("res://maps/TestMap.tscn")
+const MENU_FADE_OUT = "menu_fade_out"
 
 func _on_NewGameButton_pressed():
-  $AnimationPlayerMenu.play("menu_fade_out")
+  $AnimationPlayerMenu.play(MENU_FADE_OUT)
 
 func _on_ExitButton_pressed():
   get_tree().quit()
 
-func _on_AnimationPlayerMenu_animation_finished(_anim_name):
-  GameState.change_map("TestMap")
+func _on_AnimationPlayerMenu_animation_finished(anim_name):
+  if anim_name == MENU_FADE_OUT:
+    GameState.change_map("TestMap")
