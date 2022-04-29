8e4d9f0fcba130689bd571ba005bb9480cccd911
Telmo Valverde <telmo.valverde@tarmac.io>
2022-04-28 23:44:15 -0300

    Fading out menu on exit

--- ui/title_screen.gd
+++ ui/title_screen.gd
@@ -1,14 +1,20 @@
 extends Node
 
-var main_scene: PackedScene = load("res://maps/test_map.tscn")
 const MENU_FADE_OUT = "menu_fade_out"
+var main_scene: PackedScene = load("res://maps/test_map.tscn")
+var button_pressed: String
 
 func _on_NewGameButton_pressed():
+  button_pressed = "NewGameButton"
   $AnimationPlayerMenu.play(MENU_FADE_OUT)
 
 func _on_ExitButton_pressed():
-  get_tree().quit()
+  button_pressed = "ExitButton"
+  $AnimationPlayerMenu.play(MENU_FADE_OUT)
 
 func _on_AnimationPlayerMenu_animation_finished(anim_name):
   if anim_name == MENU_FADE_OUT:
-    GameState.change_map("test_map")
+    if button_pressed == "NewGameButton":
+      GameState.change_map("test_map")
+    elif button_pressed == "ExitButton":
+      get_tree().quit()
