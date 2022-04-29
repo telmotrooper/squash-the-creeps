b7ba54190eb616b07e2fb228d035d3e9ebf3786d
Telmo Valverde <telmo.valverde@tarmac.io>
2022-04-28 23:49:57 -0300

    Using single handler for 2 signals, to improve fade out code

--- ui/title_screen.gd
+++ ui/title_screen.gd
@@ -4,17 +4,13 @@ const MENU_FADE_OUT = "menu_fade_out"
 var main_scene: PackedScene = load("res://maps/test_map.tscn")
 var button_pressed: String
 
-func _on_NewGameButton_pressed():
-  button_pressed = "NewGameButton"
-  $AnimationPlayerMenu.play(MENU_FADE_OUT)
-
-func _on_ExitButton_pressed():
-  button_pressed = "ExitButton"
+func _on_Button_pressed(button_name):
+  button_pressed = button_name
   $AnimationPlayerMenu.play(MENU_FADE_OUT)
 
 func _on_AnimationPlayerMenu_animation_finished(anim_name):
   if anim_name == MENU_FADE_OUT:
-    if button_pressed == "NewGameButton":
+    if button_pressed == "new_game":
       GameState.change_map("test_map")
-    elif button_pressed == "ExitButton":
+    elif button_pressed == "exit":
       get_tree().quit()
