e767d758a109f931a9edcf354fe3fc315e612f70
Telmo "Trooper <telmo.trooper@gmail.com>
2022-04-30 13:57:27 -0300

    Dev (#30)
    * Working on Title Screen animations
    
    * Working on 'coming_in' animation
    
    * Working on 'coming_in' animation #2
    
    * AnimationPlayerSpaceship now has speed 1
    
    * Remove extra newline
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- ui/title_screen.gd
+++ ui/title_screen.gd
@@ -14,3 +14,7 @@ func _on_AnimationPlayerMenu_animation_finished(anim_name):
       GameState.change_map("test_map")
     elif button_pressed == "exit":
       get_tree().quit()
+
+func _on_AnimationPlayerSpaceship_animation_finished(anim_name):
+  if anim_name == "coming_in":
+    $Spatial/AnimationPlayerSpaceship.play("flying_in_space")
