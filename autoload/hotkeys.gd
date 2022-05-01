de9f2ab972b9e1e243c3819d15eeabe2cf402bc0
Telmo Valverde <telmo.valverde@tarmac.io>
2022-04-30 23:13:57 -0300

    If 'ESC' is pressed in Title Screen, close game.

--- autoload/hotkeys.gd
+++ autoload/hotkeys.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+# Actions defined in "Project > Project Settings... > Input Map".
+
 func _process(_delta):
   if Input.is_action_just_pressed("ui_toggle_fullscreen"):
     OS.window_fullscreen = !OS.window_fullscreen
@@ -9,3 +11,6 @@ func _process(_delta):
     Engine.time_scale = 2.25
   elif Input.is_action_just_released("ui_fast_forward"):
     Engine.time_scale = 1
+  
+  if get_node_or_null("/root/TitleScreen") and Input.is_action_just_pressed("ui_cancel"):
+    get_tree().quit()
