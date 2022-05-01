b51b723e27869959b00efc8289425772d9c033e7
Telmo "Trooper <telmo.trooper@gmail.com>
2022-05-01 19:09:19 -0300

    Dev (#34)
    * Updated game description
    
    * Added Godot version to title screen
    
    * Working on the pause menu to include keymaps
    
    * Renamed 'Keymaps' to 'Controls'
    
    * ESC in Controls returns to main pause menu
    
    * Added section title in pause
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- ui/pause.gd
+++ ui/pause.gd
@@ -7,43 +7,43 @@ const music_volume_text = "Music Volume: %d"
 const sound_volume_text = "Sound Volume: %d"
 
 func _ready():
-  $VBoxContainer/HBoxContainer/LeftContainer/GrassOptionButton.select(Configuration.get_value("graphics", "grass_amount"))
+  $MainPause/HBoxContainer/LeftContainer/GrassOptionButton.select(Configuration.get_value("graphics", "grass_amount"))
   
   if (GameState.MapName): # TODO: Find a way to find option from label (maybe iterate through the items?)
     if (GameState.MapName == 'height_map'):
-      $VBoxContainer/HBoxContainer/RightContainer/MapOptionButton.select(0)
+      $MainPause/HBoxContainer/RightContainer/MapOptionButton.select(0)
     elif (GameState.MapName == 'test_map'):
-      $VBoxContainer/HBoxContainer/RightContainer/MapOptionButton.select(1)
+      $MainPause/HBoxContainer/RightContainer/MapOptionButton.select(1)
   
-  $VBoxContainer/HBoxContainer/LeftContainer/DrawDistanceLabel.text = draw_distance_text % Configuration.get_value("graphics", "draw_distance")
-  $VBoxContainer/HBoxContainer/LeftContainer/DrawDistanceSlider.value = Configuration.get_value("graphics", "draw_distance")
+  $MainPause/HBoxContainer/LeftContainer/DrawDistanceLabel.text = draw_distance_text % Configuration.get_value("graphics", "draw_distance")
+  $MainPause/HBoxContainer/LeftContainer/DrawDistanceSlider.value = Configuration.get_value("graphics", "draw_distance")
   
-  $VBoxContainer/HBoxContainer/LeftContainer/SensitivityLabel.text = sensitivity_text % Configuration.get_value("controls", "mouse_sensitivity")
-  $VBoxContainer/HBoxContainer/LeftContainer/SensitivitySlider.value = Configuration.get_value("controls", "mouse_sensitivity")
+  $MainPause/HBoxContainer/LeftContainer/SensitivityLabel.text = sensitivity_text % Configuration.get_value("controls", "mouse_sensitivity")
+  $MainPause/HBoxContainer/LeftContainer/SensitivitySlider.value = Configuration.get_value("controls", "mouse_sensitivity")
   
-  $VBoxContainer/HBoxContainer/RightContainer/MusicVolumeLabel.text = music_volume_text % Configuration.get_value("audio", "music_volume")
-  $VBoxContainer/HBoxContainer/RightContainer/MusicVolumeSlider.value = Configuration.get_value("audio", "music_volume")
+  $MainPause/HBoxContainer/RightContainer/MusicVolumeLabel.text = music_volume_text % Configuration.get_value("audio", "music_volume")
+  $MainPause/HBoxContainer/RightContainer/MusicVolumeSlider.value = Configuration.get_value("audio", "music_volume")
   
-  $VBoxContainer/HBoxContainer/RightContainer/SoundVolumeLabel.text = sound_volume_text % Configuration.get_value("audio", "sound_volume")
-  $VBoxContainer/HBoxContainer/RightContainer/SoundVolumeSlider.value = Configuration.get_value("audio", "sound_volume")
+  $MainPause/HBoxContainer/RightContainer/SoundVolumeLabel.text = sound_volume_text % Configuration.get_value("audio", "sound_volume")
+  $MainPause/HBoxContainer/RightContainer/SoundVolumeSlider.value = Configuration.get_value("audio", "sound_volume")
 
 func _on_DrawDistanceSlider_value_changed(value):
   Configuration.update_setting("graphics", "draw_distance", value)
-  $VBoxContainer/HBoxContainer/LeftContainer/DrawDistanceLabel.text = draw_distance_text % value
+  $MainPause/HBoxContainer/LeftContainer/DrawDistanceLabel.text = draw_distance_text % value
   GameState.Player.set_draw_distance(value)
 
 func _on_SensitivitySlider_value_changed(value):
   Configuration.update_setting("controls", "mouse_sensitivity", value)
-  $VBoxContainer/HBoxContainer/LeftContainer/SensitivityLabel.text = sensitivity_text % value
+  $MainPause/HBoxContainer/LeftContainer/SensitivityLabel.text = sensitivity_text % value
 
 func _on_MusicVolumeSlider_value_changed(value):
   Configuration.update_setting("audio", "music_volume", value)
-  $VBoxContainer/HBoxContainer/RightContainer/MusicVolumeLabel.text = music_volume_text % value
+  $MainPause/HBoxContainer/RightContainer/MusicVolumeLabel.text = music_volume_text % value
   Configuration.set_volume("Music", value)
 
 func _on_SoundVolumeSlider_value_changed(value):
   Configuration.update_setting("audio", "sound_volume", value)
-  $VBoxContainer/HBoxContainer/RightContainer/SoundVolumeLabel.text = sound_volume_text % value
+  $MainPause/HBoxContainer/RightContainer/SoundVolumeLabel.text = sound_volume_text % value
   Configuration.set_volume("Sound", value)
 
 func pause():
@@ -55,8 +55,12 @@ func pause():
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
+  # TODO: Improve this when another menu page is needed.
   if event.is_action_pressed("pause"):
-    pause()
+    if $MainPause.visible:
+      pause()
+    else:
+      _on_ControlsBackButton_pressed()
 
 func _on_ResumeButton_pressed():
   pause()
@@ -81,6 +85,16 @@ func _on_GrassOptionButton_item_selected(index):
 
 
 func _on_MapOptionButton_item_selected(index):
-  var map_name = $VBoxContainer/HBoxContainer/RightContainer/MapOptionButton.get_item_text(index)
+  var map_name = $MainPause/HBoxContainer/RightContainer/MapOptionButton.get_item_text(index)
   GameState.change_map(map_name)
   pause()
+
+
+func _on_ControlsButton_pressed():
+  $MainPause.visible = false
+  $PauseControls.visible = true
+
+
+func _on_ControlsBackButton_pressed():
+  $MainPause.visible = true
+  $PauseControls.visible = false
