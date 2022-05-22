2a74bbbeb95116beedf53a3e97e90aa3169525b5
Telmo Valverde <telmo.valverde@tarmac.io>
2022-05-21 21:35:00 -0300

    Make a sound when draining water

--- maps/mountain_map.gd
+++ maps/mountain_map.gd
@@ -1,4 +1,9 @@
 extends Spatial
 
+export (AudioStream) var drain_water_sound
+
 func _on_RedButton_pressed():
   $AnimationPlayer.play("drain_water")
+
+func play_drain_water_sound():
+  GameState.play_audio(drain_water_sound)
