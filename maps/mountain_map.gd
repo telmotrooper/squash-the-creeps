dae25558103e0b116f354f6cfcf3b332660a4c03
Telmo Valverde <telmo.valverde@tarmac.io>
2022-05-21 21:45:36 -0300

    Made water drain quicker; Stop water sound as soon as it's completely drained

--- maps/mountain_map.gd
+++ maps/mountain_map.gd
@@ -4,6 +4,3 @@ export (AudioStream) var drain_water_sound
 
 func _on_RedButton_pressed():
   $AnimationPlayer.play("drain_water")
-
-func play_drain_water_sound():
-  GameState.play_audio(drain_water_sound)
