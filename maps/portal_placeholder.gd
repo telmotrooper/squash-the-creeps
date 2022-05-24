c19233e68a392f9333469201e186fdc0c10e4eed
Telmo Valverde <telmo.valverde@tarmac.io>
2022-05-24 00:30:35 -0300

    Player shrinks when he enters a portal

--- maps/portal_placeholder.gd
+++ maps/portal_placeholder.gd
@@ -3,4 +3,5 @@ extends CSGCylinder
 export var map_name: String
 
 func _on_Portal_entered(_body):
+  GameState.Player.get_node("EffectsAnimationPlayer").play("shrink")
   GameState.change_map(map_name)
