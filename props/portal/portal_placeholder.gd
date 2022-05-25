8e94b9757097c7b6f6945ef7c22dfc801bf0faab
Telmo Valverde <telmo.valverde@tarmac.io>
2022-05-25 16:42:28 -0300

    Moved portal to new folder 'props' and added logic to play sound on teleport. No sound ready yet.

--- props/portal/portal_placeholder.gd
+++ props/portal/portal_placeholder.gd
@@ -3,5 +3,6 @@ extends CSGCylinder
 export var map_name: String
 
 func _on_Portal_entered(_body):
+  #$AudioStreamPlayer.play()
   GameState.Player.get_node("EffectsAnimationPlayer").play("shrink")
   GameState.change_map(map_name)
