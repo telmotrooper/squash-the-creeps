224e788a5c7118e5639f299298a216e76fd5f8b5
Telmo Valverde <telmo.valverde@tarmac.io>
2022-05-31 01:01:57 -0300

    Clean up collectables on new game

--- ui/title_screen.gd
+++ ui/title_screen.gd
@@ -18,6 +18,8 @@ func _on_AnimationPlayerMenu_animation_finished(anim_name):
       GameState.MapName = "test_map"
       if is_instance_valid($"/root/Main"):
         $"/root/Main".load_world(new_game_scene.get_path())
+        GameState.godot_heads_counter = 0
+        GameState.godot_heads_collected = {}
       else:
         var _error = get_tree().change_scene(new_game_scene.get_path())
     elif button_pressed == "exit":
