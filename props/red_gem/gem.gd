2a06fedcdc05191ae62fc3e19d793bb581d972f1
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-05 23:49:39 -0300

    Make gem disappear when it's too close to player

--- props/red_gem/gem.gd
+++ props/red_gem/gem.gd
@@ -4,19 +4,20 @@ class_name Gem
 var follow_player := false
 var t := 0.0
 
+# https://docs.godotengine.org/en/stable/tutorials/math/interpolation.html
 func _physics_process(delta):
-  # https://docs.godotengine.org/en/stable/tutorials/math/interpolation.html
-  if follow_player: #and t < 1.0:
+  if follow_player and is_instance_valid(GameState.Player):
     var player_position = GameState.Player.global_transform.origin
     var gem_position = self.global_transform.origin
     
-    t += delta
     var distance = gem_position.distance_to(player_position)
     
-    print("t: %f distance: %f" % [t, distance])
-    
+    # Make gem follow player.
+    t += delta * 0.25
     self.global_transform.origin = gem_position.linear_interpolate(player_position, t)
+    
+    if distance <= 0.8 or t >= 1.0:
+      queue_free()
 
-
-func _on_GrabArea_body_entered(body):
+func _on_GrabArea_body_entered(_body):
   follow_player = true
