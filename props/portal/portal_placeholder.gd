ee7d06bbff11eedf63dbd2dd44e313ce5701d17c
Telmo "Trooper <telmo.trooper@gmail.com>
2022-06-07 23:22:36 -0300

    feat/portal-requirements (#77)
    * Implemented initial version of portals locked by number of Godot Heads collected
    
    * Outline in labels, portal shows hud when requirement is not met
    
    * Changed text for Godot Heads requirement
    
    * Adjusted render priority for 3D labels in portals
    
    * Split 'map_name' and 'label' in portals
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- props/portal/portal_placeholder.gd
+++ props/portal/portal_placeholder.gd
@@ -1,12 +1,23 @@
 extends CSGCylinder
 
+export var label: String
 export var map_name: String
+export var godot_heads_required: int
 
 func _ready():
-  $Label3D.text = map_name
+  $Label3D.text = label if label else map_name
+  
+  if godot_heads_required >= 1:
+    $RequirementLabel.text = "Godot Heads x %d" % godot_heads_required
+  else:
+    $RequirementLabel.text = ""
 
 func _on_Portal_entered(_body):
-  $AnimationPlayer.play("shrink")
-  #$AudioStreamPlayer.play()
-  GameState.Player.get_node("EffectsAnimationPlayer").play("shrink")
-  GameState.change_map(map_name)
+  if GameState.global_progress.collected >= godot_heads_required:
+    $AnimationPlayer.play("shrink")
+    $RequirementLabel.visible = false
+    #$AudioStreamPlayer.play()
+    GameState.Player.get_node("EffectsAnimationPlayer").play("shrink")
+    GameState.change_map(map_name)
+  else:
+    GameState.UserInterface.show_hud()
