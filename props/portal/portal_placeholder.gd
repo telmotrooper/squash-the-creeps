1e1625f6002ac6aeb6f490dfda52b502df4b8c6f
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-07 23:20:26 -0300

    Split 'map_name' and 'label' in portals

--- props/portal/portal_placeholder.gd
+++ props/portal/portal_placeholder.gd
@@ -1,10 +1,11 @@
 extends CSGCylinder
 
+export var label: String
 export var map_name: String
 export var godot_heads_required: int
 
 func _ready():
-  $Label3D.text = map_name
+  $Label3D.text = label if label else map_name
   
   if godot_heads_required >= 1:
     $RequirementLabel.text = "Godot Heads x %d" % godot_heads_required
