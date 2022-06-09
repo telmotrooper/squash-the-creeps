c9eb3db55dde383084a5c425467600482c7f7bfb
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-08 22:00:02 -0300

    Reorganizing nodes in TestMap

--- maps/test_map.gd
+++ maps/test_map.gd
@@ -1,30 +1,11 @@
 extends Node
 
-export (PackedScene) var enemy_scene
-export var spawn_enemies := true
-
 func _ready():
   GameState.RetryCamera = $RetryCamera
-  GameState.Grass = $Map/Grass
+  GameState.Grass = get_node("%GreenPlatformGrass")
   GameState.update_grass()
-  
-  if !spawn_enemies:
-    $EnemyTimer.stop()
-  randomize()
-
-func _on_EnemyTimer_timeout():
-  var enemy = enemy_scene.instance()
-  
-  var enemy_spawn_location = $SpawnPath/SpawnLocation
-  enemy_spawn_location.unit_offset = randf()
-  
-  var player_position = $Player.transform.origin
-  
-  add_child(enemy)
-  enemy.initiliaze(enemy_spawn_location.translation, player_position)
 
 func _on_Player_hit():
-  $EnemyTimer.stop()
   GameState.UserInterface.retry()
 
 func _on_RedButton_pressed():
