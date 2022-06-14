d8de6e76a2a5b7b02cbbd28a9ecf22e862115a1f
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-14 11:07:19 -0300

    Added a couple comments to 'camera.gd'

--- entities/player/camera.gd
+++ entities/player/camera.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-var horizontal =  0
+var horizontal = 0
 var vertical = 0
 var v_min = -70 # Looking up
 var v_max = 12 # Look down
@@ -21,8 +21,10 @@ func _input(event):
 
 func _physics_process(delta):
   #print(vertical) # It's useful to print the current value when trying to find the proper values for 'min' and 'max'.
-  vertical = clamp(vertical, v_min, v_max)
   
+  # Lock "vertical" position in range [v_min, v_max].
+  vertical = clamp(vertical, v_min, v_max)
+
+  # Move camera smoothly based on "delta * acceleration".
   $Horizontal.rotation_degrees.y = lerp($Horizontal.rotation_degrees.y, horizontal, delta * h_acceleration)
   $Horizontal/Vertical.rotation_degrees.x = lerp($Horizontal/Vertical.rotation_degrees.x, vertical, delta * v_acceleration)
-  
