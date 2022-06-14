6cf47130119be6d0d3857a77cc611cfee4b98360
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-14 01:23:17 -0300

    Sprint tutorial implemented with falling bridge

--- maps/test_map.gd
+++ maps/test_map.gd
@@ -10,3 +10,6 @@ func _on_Player_hit():
 
 func _on_RedButton_pressed():
   $Map/MovingPlatforms/Manual.move_platforms()
+
+func _on_SprintTutorial_body_entered(_body):
+  get_node("%SprintTutorial").get_node("AnimationPlayer").play("show_sprint_label")
