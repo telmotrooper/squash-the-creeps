385bbd71a9345475ed40734bea825b1c56fb16bf
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-14 13:33:40 -0300

    Implemented camera zoom

--- entities/player/camera.gd
+++ entities/player/camera.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 const ZOOM_STEP := 0.5
+var min_zoom := 9
+var max_zoom := 18
 
 var horizontal := 0
 var vertical := 0
@@ -8,7 +10,8 @@ var v_min := -70 # Looking up
 var v_max := 12 # Look down
 var h_acceleration := 10
 var v_acceleration := 10
-var default_camera_zoom := 10
+
+#var default_camera_zoom := 10
 
 func _ready():
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -18,17 +21,18 @@ func _ready():
   $Horizontal/Vertical/ClippedCamera.far = Configuration.get_value("graphics", "draw_distance")
 
 func _input(event):
-  print(get_node("%ClippedCamera").translation.z)
+  var zoom = get_node("%ClippedCamera").translation.z  
+
   if event is InputEventMouseMotion:
     horizontal -= event.relative.x * Configuration.get_value("controls", "mouse_sensitivity")
     vertical -= event.relative.y * Configuration.get_value("controls", "mouse_sensitivity")
-  elif event.is_action_pressed("zoom_in"):
+  elif event.is_action_pressed("zoom_in") and zoom > min_zoom:
     get_node("%ClippedCamera").translation.z -= ZOOM_STEP
-  elif event.is_action_pressed("zoom_out"):
+  elif event.is_action_pressed("zoom_out") and zoom < max_zoom:
     get_node("%ClippedCamera").translation.z += ZOOM_STEP
 
 func _physics_process(delta):
-  #print(vertical) # It's useful to print the current value when trying to find the proper values for 'min' and 'max'.
+  # print(vertical) # It's useful to print the current value when trying to find the proper values for 'min' and 'max'.
   
   # Lock "vertical" position in range [v_min, v_max].
   vertical = clamp(vertical, v_min, v_max)
