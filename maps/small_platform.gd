ede23e88c1e0c82e02b1b19e5388347dbb1cf65d
Telmo Valverde <telmo.valverde@tarmac.io>
2022-06-16 23:45:12 -0300

    Tweaking tween

--- maps/small_platform.gd
+++ maps/small_platform.gd
@@ -2,30 +2,34 @@ extends StaticBody
 
 onready var tween = get_node("Tween")
 
-var duration: float = 3
+export var duration: float = 3
+export var move_y: float = -1
+
+var transition_type := Tween.TRANS_BACK
 var initial_y: float
 var final_y: float
 var last
 
 func _ready():
   initial_y = translation.y
-  final_y = translation.y - 1
+  final_y = translation.y + move_y
   
-  go_down()
+  go_final()
 
+func go_final():
+  last = "final"
+  tween.interpolate_property(self, "translation:y", null,
+    final_y, duration, transition_type)
+  tween.start()
 
-func _on_Tween_tween_completed(object, key):
-  if last == "down":
-    go_up()
+func go_initial():
+  last = "initial"
+  tween.interpolate_property(self, "translation:y", null,
+    initial_y, duration, transition_type)
+  tween.start()
+
+func _on_Tween_tween_completed(_object, _key):
+  if last == "final":
+    go_initial()
   else:
-    go_down()
-
-func go_down():
-  last = "down"
-  tween.interpolate_property(self, "translation:y", null, final_y, duration, Tween.TRANS_BACK)
-  tween.start()
-
-func go_up():
-  last = "up"
-  tween.interpolate_property(self, "translation:y", null, initial_y, duration, Tween.TRANS_BACK)
-  tween.start()
+    go_final()
