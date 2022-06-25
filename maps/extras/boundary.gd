90eeef53f5f45d1c810e8abe58bdbbf14a25fc5c
Telmo "Trooper <telmo.trooper@gmail.com>
2022-06-25 19:19:18 -0300

    misc/adjustments-2 (#108)
    * Enemies die if they hit the boundary
    
    * When loading a scene without a default camera, load fallback scene instead
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- maps/extras/boundary.gd
+++ maps/extras/boundary.gd
@@ -1,4 +1,7 @@
 extends Area
 
-func _on_Boundary_body_entered(_body):
-  get_tree().call_group("players", "die")
+func _on_Boundary_body_entered(body):
+  if body is Player:
+    get_tree().call_group("players", "die")
+  elif body is Enemy:
+    body.kill()
