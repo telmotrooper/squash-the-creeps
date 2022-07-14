21302072187d76946e26e7187128f45ab7ad8dd0
Telmo "Trooper <telmo.trooper@gmail.com>
2022-07-14 00:02:41 -0300

    feat/improve-mountain-map (#114)
    * Adjusting button in mountain map
    
    * Adjusting button in mountain map #2
    
    * Water in mountain map kills player
    
    * Moved gems in lake a bit deeper
    
    * Added gems close to hole
    
    * Adjusted 'total' to account for new gems
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- maps/extras/boundary.gd
+++ maps/extras/boundary.gd
@@ -1,4 +1,13 @@
+tool
 extends Area
+
+export (bool) var mesh_visible setget set_visible, get_visible
+
+func set_visible(value):
+  $MeshInstance.visible = value
+
+func get_visible():
+  return $MeshInstance.visible
 
 func _on_Boundary_body_entered(body):
   if body is Player:
