e9727ccf85d9f51c81e1ee1172b1cb00667dfccd
Telmo "Trooper <telmo.trooper@gmail.com>
2022-07-20 00:29:35 -0300

    feat/floating-particles (#119)
    * Added default environment to project
    
    * Particles in tunnel
    
    * Making player float
    
    * Added logic to stop floating
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- maps/lake_map.gd
+++ maps/lake_map.gd
@@ -12,3 +12,25 @@ func _on_RedButton_pressed():
 
 func _on_Player_hit():
   GameState.UserInterface.retry()
+
+func emit_particles(value: bool):
+  get_node("%TunnelFloatingParticles").emitting = value
+  get_node("%TunnelFloatingParticles").visible = value
+
+func _on_AreaToStartParticles_body_entered(_body):
+  emit_particles(true)
+
+func _on_AreaToStopParticles_body_entered(player):
+  if player.is_on_floor():
+    emit_particles(false)
+
+func _on_AreaToStopParticles_body_exited(player):
+  if player.is_on_floor():
+    emit_particles(false)
+
+func _on_AreaToMakePlayerFloat_body_entered(_body):
+  if get_node("%TunnelFloatingParticles").emitting:
+    GameState.Player.floating = true
+
+func _on_AreaToMakePlayerFloat_body_exited(_body):
+  GameState.Player.floating = false
