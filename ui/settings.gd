d8835aae377c6dfa84a3cac1c999935aec1f664d
Telmo Valverde <telmo.valverde@tarmac.io>
2022-07-24 18:45:39 -0300

    Moved 'Settings' to its own node

---
+++ ui/settings.gd
@@ -0,0 +1,48 @@
+extends VBoxContainer
+
+const draw_distance_text = "Draw Distance: %d"
+const sensitivity_text = "Mouse Sensitivity: %.2f"
+const music_volume_text = "Music Volume: %d"
+const sound_volume_text = "Sound Volume: %d"
+
+func _ready():
+  get_node("%GrassOptionButton").select(Configuration.get_value("graphics", "grass_amount"))
+
+  if GameState.MapName: # TODO: Find a way to find option from label (maybe iterate through the items?)
+    if GameState.MapName == 'hub_1':
+      get_node("%MapOptionButton").select(0)
+    elif (GameState.MapName == 'test_map'):
+      get_node("%MapOptionButton").select(1)
+    elif (GameState.MapName == 'lake_map'):
+      get_node("%MapOptionButton").select(2)
+  
+  get_node("%DrawDistanceLabel").text = draw_distance_text % Configuration.get_value("graphics", "draw_distance")
+  get_node("%DrawDistanceSlider").value = Configuration.get_value("graphics", "draw_distance")
+  
+  get_node("%SensitivityLabel").text = sensitivity_text % Configuration.get_value("controls", "mouse_sensitivity")
+  get_node("%SensitivitySlider").value = Configuration.get_value("controls", "mouse_sensitivity")
+  
+  get_node("%MusicVolumeLabel").text = music_volume_text % Configuration.get_value("audio", "music_volume")
+  get_node("%MusicVolumeSlider").value = Configuration.get_value("audio", "music_volume")
+  
+  get_node("%SoundVolumeLabel").text = sound_volume_text % Configuration.get_value("audio", "sound_volume")
+  get_node("%SoundVolumeSlider").value = Configuration.get_value("audio", "sound_volume")
+
+func _on_DrawDistanceSlider_value_changed(value):
+  Configuration.update_setting("graphics", "draw_distance", value)
+  get_node("%DrawDistanceLabel").text = draw_distance_text % value
+  GameState.Player.set_draw_distance(value)
+
+func _on_SensitivitySlider_value_changed(value):
+  Configuration.update_setting("controls", "mouse_sensitivity", value)
+  get_node("%SensitivityLabel").text = sensitivity_text % value
+
+func _on_MusicVolumeSlider_value_changed(value):
+  Configuration.update_setting("audio", "music_volume", value)
+  get_node("%MusicVolumeLabel").text = music_volume_text % value
+  Configuration.set_volume("Music", value)
+
+func _on_SoundVolumeSlider_value_changed(value):
+  Configuration.update_setting("audio", "sound_volume", value)
+  get_node("%SoundVolumeLabel").text = sound_volume_text % value
+  Configuration.set_volume("Sound", value)
