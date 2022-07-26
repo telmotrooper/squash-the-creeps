711255d87931d1ca5be0b04fc0a192f79248465e
Telmo Valverde <telmo.valverde@tarmac.io>
2022-07-25 22:50:17 -0300

    In Title Screen, pressing 'Esc' on Settings returns to main menu

--- maps/hub_1.gd
+++ maps/hub_1.gd
@@ -4,7 +4,7 @@ export (Environment) var day_environment
 export (Environment) var night_environment
 export (AudioStream) var map_music
 
-func _ready():
+func _ready() -> void:
   GameState.stop_music()
   if GameState.hub_1_at_night:
     $WorldEnvironment.environment = night_environment
@@ -16,8 +16,8 @@ func _ready():
   
   GameState.RetryCamera = $RetryCamera
 
-func _on_Player_hit():
+func _on_Player_hit() -> void:
   GameState.UserInterface.retry()
 
-func _on_AudioStreamPlayer_finished():
+func _on_AudioStreamPlayer_finished() -> void:
   GameState.play_music(map_music)
