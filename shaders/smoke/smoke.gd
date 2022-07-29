5603530768880bdbf39e0a7929ac6baea1173c5e
Telmo Valverde <telmo.valverde@tarmac.io>
2022-07-28 21:39:58 -0300

    Smoke always faces the current camera

--- shaders/smoke/smoke.gd
+++ shaders/smoke/smoke.gd
@@ -1,8 +1,11 @@
+tool
 extends MeshInstance
 
+var current_camera: Camera
+
 func _process(_delta):
-  if is_instance_valid(GameState.Player):  
-    var player_camera = GameState.Player.get_node("%ClippedCamera")
-    var copy_of_camera_position = player_camera.global_transform.origin
+  current_camera = get_viewport().get_camera()
+  if is_instance_valid(current_camera):  
+    var copy_of_camera_position = current_camera.global_transform.origin
     copy_of_camera_position.y = 100
     look_at(copy_of_camera_position, Vector3.UP)
