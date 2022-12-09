c02b69264d785eb08270ed14b78c4e50f822515b
Telmo Valverde <telmo.valverde@tarmac.io>
2022-12-09 15:35:01 -0300

    Added typings to 'user_interface.gd' and adjusted logic for announcements

--- ui/user_interface.gd
+++ ui/user_interface.gd
@@ -2,7 +2,7 @@ extends Control
 
 var hud_visible := false
 
-func _ready():
+func _ready() -> void:
   # If every map is gonna have its own UserInterface instance,
   # we'll need a reference to the current one.
   GameState.UserInterface = self
@@ -19,40 +19,39 @@ func _ready():
   GameState.generate_progress_report(owner.name)
   get_node("%GemLabel").text = "%d" % GameState.amount_of_gems
 
-func _process(_delta):
+func _process(_delta: float) -> void:
   $FPSLabel.text = "FPS: %s" % Engine.get_frames_per_second()
 
 func _unhandled_input(event):
   if $Retry.visible and event.is_action_pressed("ui_accept"):
     GameState.reload_current_scene()
 
-func retry():
+func retry() -> void:
   $Retry.show()
   GameState.RetryCamera.current = true
 
-func show_hud():
+func show_hud() -> void:
   $HUDTimer.start()
   if not hud_visible:
     $HUDAnimationPlayer.play_backwards("hide_hud")
     hud_visible = true
 
-func hide_hud(): # Triggered by HUDTimer.
+func hide_hud() -> void: # Triggered by HUDTimer.
   if hud_visible:
     $HUDAnimationPlayer.play("hide_hud")
     hud_visible = false
 
-func show_all_godot_heads_collected():
-  if $AnnouncementAnimationPlayer.is_playing():
-    # If another announcement is underway, wait 5 seconds.
-    var timer = get_tree().create_timer(5, false)
-    yield(timer, "timeout")
-  $Announcement.text = "100% Godot Heads collected"
-  $AnnouncementAnimationPlayer.play("show_all_godot_heads_collected")
+func show_all_godot_heads_collected() -> void:
+  show_announcement("100% Godot Heads collected")
 
-func show_all_gems_collected():
+func show_all_gems_collected() -> void:
+  show_announcement("100% gems collected")
+
+func show_announcement(text: String) -> void:
   if $AnnouncementAnimationPlayer.is_playing():
-    # If another announcement is underway, wait 5 seconds.
+    # If another announcement is underway, wait 5 seconds and try again.
     var timer = get_tree().create_timer(5, false)
     yield(timer, "timeout")
-  $Announcement.text = "100% gems collected"
+  
+  $Announcement.text = text
   $AnnouncementAnimationPlayer.play("show_all_godot_heads_collected")
