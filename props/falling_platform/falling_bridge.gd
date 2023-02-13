4d64b04483329a6eebee9bbcad512e13f9a8e285
Telmo "Trooper <telmo.trooper@gmail.com>
2023-02-13 19:30:57 -0300

    Bridge resets when player dies (#143)
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- props/falling_platform/falling_bridge.gd
+++ props/falling_platform/falling_bridge.gd
@@ -15,3 +15,11 @@ func _on_Timer_timeout() -> void:
   if index < platforms.size():
     platforms[index].falling = true
     index += 1
+  else: # No more platforms to fall.
+    $Timer.stop()
+
+func reset() -> void:
+  index = 0
+  $Timer.stop()
+  for platform in $Platforms.get_children():
+    platform.reset()
