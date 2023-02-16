1e9d6cc12bfcc50b2ca69e87934e43fff1709723
Telmo Valverde <telmo.valverde@tarmac.io>
2023-02-16 11:04:51 -0300

    Fixed collision layer in height map

--- maps/extras/height_map/height_map.gd
+++ maps/extras/height_map/height_map.gd
@@ -10,5 +10,5 @@ func _ready() -> void:
   $height_map/Hole/Hole.add_to_group("breakable_floor")
 
 func setup_collision_layers(node: Node) -> void:
-  node.set_collision_layer_value(0, false) # Remove from "Player"
-  node.set_collision_layer_value(2, true)  # Add to "World3D"
+  node.set_collision_layer_value(1, false) # Remove from "Player"
+  node.set_collision_layer_value(3, true)  # Add to "World (Static)"
