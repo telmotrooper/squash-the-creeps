6d6904e2a5ad180703997d15363633dfbcace67d
Telmo Valverde <telmo.valverde@tarmac.io>
2023-02-15 23:46:28 -0300

    Fixed Tween logic in 'small_platform'

--- maps/extras/small_platform/small_platform.gd
+++ maps/extras/small_platform/small_platform.gd
@@ -1,35 +1,17 @@
 extends StaticBody3D
 
-@onready var tween = get_node("Tween")
-
 @export var duration: float = 3
 @export var move_y: float = -1
 
-var transition_type := Tween.TRANS_BACK
 var initial_y: float
 var final_y: float
-var last: String
+var tween: Tween
 
 func _ready() -> void:
   initial_y = position.y
   final_y = position.y + move_y
   
-  go_final()
+  tween = create_tween().set_loops().set_trans(Tween.TRANS_BACK)
 
-func go_final() -> void:
-  last = "final"
-#  tween.interpolate_property(self, "position:y", null,
-#    final_y, duration, transition_type)
-#  tween.start()
-
-func go_initial() -> void:
-  last = "initial"
-#  tween.interpolate_property(self, "position:y", null,
-#    initial_y, duration, transition_type)
-#  tween.start()
-
-func _on_Tween_tween_completed(_object: Object, _key: NodePath) -> void:
-  if last == "final":
-    go_initial()
-  else:
-    go_final()
+  tween.tween_property(self, "position:y", final_y, duration)
+  tween.tween_property(self, "position:y", initial_y, duration)
