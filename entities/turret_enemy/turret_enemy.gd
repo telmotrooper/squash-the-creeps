fdfdc5937208701ee06d91fb7b7d132b17803d27
Telmo Valverde <telmo.valverde@tarmac.io>
2023-02-18 14:06:55 -0300

    Turret now follows player

--- entities/turret_enemy/turret_enemy.gd
+++ entities/turret_enemy/turret_enemy.gd
@@ -4,19 +4,25 @@ enum { IDLE, ALERT, ATTACKING }
 
 var state = IDLE
 
-func _ready() -> void:
-  pass
-
 func _physics_process(_delta: float) -> void:
   match state:
     IDLE:
       pass
     ALERT:
-      pass
+      $ExclamationMark.visible = true
+      $AlertTimer.start()
+      set_physics_process(false)
     ATTACKING:
-      pass
-
+      if is_instance_valid(GameState.Player):
+        aim_at_player(GameState.Player.transform.origin)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-  print("body entered")
   state = ALERT
+
+func aim_at_player(player_position) -> void:
+  look_at(player_position, Vector3.UP)
+
+func _on_alert_timer_timeout() -> void:
+  $ExclamationMark.visible = false
+  state = ATTACKING
+  set_physics_process(true)
