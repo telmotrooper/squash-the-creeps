a553ef0726217babaf0f53efe629e3c8303634b9
Telmo "Trooper <telmo.trooper@gmail.com>
2023-02-18 23:15:09 -0300

    Added "turret enemy" (#148)
    * Added new area for future 'tank enemy'
    
    * Working on 'turret enemy'
    
    * Added enemy to turret enemy
    
    * Added cockpit floor
    
    * Added detection area to 'turret enemy'
    
    * Turret now follows player
    
    * Added timer to shoot
    
    * Adjusted logic so enemy doesn't become alert more than once
    
    * Implementing bullet
    
    * Trigger first shot immediately
    
    * Testing changes in bullet
    
    * Refactoring bullet
    
    * Using 'delta' in bullet movement
    
    * Made bullet spawn closer to cannon
    
    * Added TODO
    
    * Delete bullet after 5 seconds
    
    ---------
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

---
+++ entities/turret_enemy/turret_enemy.gd
@@ -0,0 +1,43 @@
+extends Node3D
+class_name TurretEnemy
+
+@export var bullet_scene: PackedScene
+
+enum { IDLE, ALERT, ATTACKING }
+
+var state = IDLE
+
+func _physics_process(_delta: float) -> void:
+  match state:
+    IDLE:
+      pass
+    ALERT:
+      $ExclamationMark.visible = true
+      $AlertTimer.start()
+      set_physics_process(false)
+    ATTACKING:
+      if is_instance_valid(GameState.Player):
+        aim_at_player(GameState.Player.transform.origin)
+        
+        if $GunTimer.is_stopped():
+          _on_gun_timer_timeout() # Trigger first shot immediately.
+          $GunTimer.start()      
+
+func _on_area_3d_body_entered(_body: Node3D) -> void:
+  if state == IDLE:
+    state = ALERT
+
+func aim_at_player(player_position) -> void:
+  look_at(player_position, Vector3.UP)
+
+func _on_alert_timer_timeout() -> void:
+  $ExclamationMark.visible = false
+  state = ATTACKING
+  set_physics_process(true)
+
+func _on_gun_timer_timeout() -> void:
+  if is_instance_valid(GameState.Player):
+    # The position (Vector3) passed to the bullet is an approximation
+    # of where we want it to spawn relative to this node.
+    var bullet = bullet_scene.instantiate().setup(Vector3(0,3,-10), GameState.Player.transform.origin)
+    add_child(bullet)
