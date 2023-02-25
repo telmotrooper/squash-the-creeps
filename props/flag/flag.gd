276bcd0e805fcb64df01285855c809e554135142
Telmo Valverde <telmo.valverde@tarmac.io>
2023-02-24 23:15:50 -0300

    Displaying message based on area

--- props/flag/flag.gd
+++ props/flag/flag.gd
@@ -3,8 +3,13 @@ extends Node3D
 var text_index = 0
 var text = "CHECKPOINT"
 
+func _ready() -> void:
+  $Label3D.hide()
+
 func _on_text_timer_timeout() -> void:
   $Label3D.text = text.substr(0, text_index)
+  $Label3D.show()
+  
   text_index += 1
   if text_index > text.length():
     $TextTimer.stop()
@@ -13,3 +18,12 @@ func _on_text_timer_timeout() -> void:
 func _on_restart_timer_timeout() -> void:
   text_index = 0
   $TextTimer.start()
+
+func _on_area_3d_body_entered(_body: Node3D) -> void:
+  $TextTimer.start()
+
+func _on_area_3d_body_exited(_body: Node3D) -> void:
+  $Label3D.hide()
+  $TextTimer.stop()
+  $RestartTimer.stop()
+  text_index = 0
