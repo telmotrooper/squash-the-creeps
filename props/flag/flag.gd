c1f55f77d14d6fc12d50d1359f8be17a484258f9
Telmo Valverde <telmo.valverde@tarmac.io>
2023-02-24 23:58:34 -0300

    When animation finishes it starts the timer

--- props/flag/flag.gd
+++ props/flag/flag.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var material_1: Material
 @export var material_2: Material
 
-var text_index = 0
+var text_index = 1
 var text = "CHECKPOINT"
 var initial_flag_y: float
 
@@ -27,8 +27,7 @@ func _on_area_3d_body_entered(_body: Node3D) -> void:
   tween.tween_property($Flag, "position:y", -4, 1)
   tween.tween_callback($Flag.set_surface_override_material.bind(0, material_2))
   tween.tween_property($Flag, "position:y", initial_flag_y, 1)
-  
-  $TextTimer.start()
+  tween.tween_callback($TextTimer.start)
 
 func _on_visibility_timer_timeout() -> void:
   $Label3D.hide()
