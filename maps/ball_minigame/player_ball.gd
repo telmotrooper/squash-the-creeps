bd24cad71dedf8c3a487f640eef204ba8d799cda
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-01 13:42:58 -0300

    Added camera to scene

--- maps/ball_minigame/player_ball.gd
+++ maps/ball_minigame/player_ball.gd
@@ -2,9 +2,15 @@ extends RigidBody3D
 
 # Reference: https://youtu.be/G6OGM4fdF3M
 
+var initial_camera_position: Vector3
 var force = 40
 
-func _process(delta: float) -> void:
+func _ready() -> void:
+  initial_camera_position = %Camera3D.position
+
+func _physics_process(delta: float) -> void:
+  %Camera3D.position = initial_camera_position + position
+
   if Input.is_action_pressed("move_forward"):
     self.angular_velocity.x -= force * delta
   elif Input.is_action_pressed("move_back"):
