468901b7f97acb0bd3f07b80c4c666628db68514
Telmo "Trooper <telmo.trooper@gmail.com>
2023-03-02 22:45:05 -0300

    Dev (#161)
    * Adjusting particle behavior in LakeMap
    
    * Adjusted size of emission sphere
    
    * Added breakable wall with decal
    
    * Fixed collision shape of 'ball'
    
    * Ball should collide with 'World (dynamic)' layer
    
    * Material automatically updated
    
    * Run physics on a separate thread
    
    * Moved 'Crack_1.png' to 'assets' and fixed boundary for small platforms
    
    * Added 'Crack_1' to 'CREDITS.md'
    
    ---------
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -211,6 +211,8 @@ func _physics_process(delta: float) -> void:
         entity.squash()
       elif entity is RedButton:
         entity.press()
+      elif entity.is_in_group("breakable_scenario"):
+        entity.queue_free()
   
   # Prevent the player from going too high when bouncing unchecked a slope.
   if velocity.y > bounce_cap:
