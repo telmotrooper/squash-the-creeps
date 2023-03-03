89b1fae154be459cb582c6d48a3712ad439905bd
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-03 14:55:33 -0300

    Adjusting Dialogic theme

--- addons/dialogic/Events/DefaultLayouts/Default/DialogicDefaultLayout.gd
+++ addons/dialogic/Events/DefaultLayouts/Default/DialogicDefaultLayout.gd
@@ -26,39 +26,39 @@ enum Alignments {Left, Center, Right}
 
 ## FOR TESTING PURPOSES
 func _ready():
-	add_to_group('dialogic_main_node')
-	
-	## FONT SETTINGS
-	%DialogicNode_DialogText.alignment = text_alignment
-	
-	%DialogicNode_DialogText.add_theme_font_size_override("normal_font_size", text_size)
-	%DialogicNode_DialogText.add_theme_font_size_override("bold_font_size", text_size)
-	%DialogicNode_DialogText.add_theme_font_size_override("italics_font_size", text_size)
-	%DialogicNode_DialogText.add_theme_font_size_override("bold_italics_font_size", text_size)
-	
-	%DialogicNode_DialogText.add_theme_color_override("default_color", text_color)
-	
-	if !normal_font.is_empty():
-		%DialogicNode_DialogText.add_theme_font_override("normal_font", load(normal_font))
-	if !bold_font.is_empty():
-		%DialogicNode_DialogText.add_theme_font_override("bold_font", load(bold_font))
-	if !italic_font.is_empty():
-		%DialogicNode_DialogText.add_theme_font_override("italitc_font", load(italic_font))
-	if !bold_italic_font.is_empty():
-		%DialogicNode_DialogText.add_theme_font_override("bold_italics_font", load(bold_italic_font))
-	
-	## BOX SETTINGS
-	%DialogTextPanel.self_modulate = box_modulate
-	%TextInputPanel.self_modulate = box_modulate
-	
-	## NAME LABEL SETTINGS
-	%DialogicNode_NameLabel.add_theme_font_size_override("font_size", name_label_font_size)
-	
-	if !name_label_font.is_empty():
-		%DialogicNode_NameLabel.add_theme_font_override('font', load(name_label_font))
-	
-	%DialogicNode_NameLabel.add_theme_color_override("font_color", name_label_color)
-	
-	%DialogicNode_NameLabel.use_character_color = name_label_use_character_color
-	
-	%NameLabelPanel.self_modulate = name_label_box_modulate
+  add_to_group('dialogic_main_node')
+  
+  ## FONT SETTINGS
+  %DialogicNode_DialogText.alignment = text_alignment
+  
+  %DialogicNode_DialogText.add_theme_font_size_override("normal_font_size", text_size)
+  %DialogicNode_DialogText.add_theme_font_size_override("bold_font_size", text_size)
+  %DialogicNode_DialogText.add_theme_font_size_override("italics_font_size", text_size)
+  %DialogicNode_DialogText.add_theme_font_size_override("bold_italics_font_size", text_size)
+  
+  %DialogicNode_DialogText.add_theme_color_override("default_color", text_color)
+  
+  if !normal_font.is_empty():
+    %DialogicNode_DialogText.add_theme_font_override("normal_font", load(normal_font))
+  if !bold_font.is_empty():
+    %DialogicNode_DialogText.add_theme_font_override("bold_font", load(bold_font))
+  if !italic_font.is_empty():
+    %DialogicNode_DialogText.add_theme_font_override("italitc_font", load(italic_font))
+  if !bold_italic_font.is_empty():
+    %DialogicNode_DialogText.add_theme_font_override("bold_italics_font", load(bold_italic_font))
+  
+  ## BOX SETTINGS
+  %DialogTextPanel.self_modulate = box_modulate
+  %TextInputPanel.self_modulate = box_modulate
+  
+  ## NAME LABEL SETTINGS
+  %DialogicNode_NameLabel.add_theme_font_size_override("font_size", name_label_font_size)
+  
+  if !name_label_font.is_empty():
+    %DialogicNode_NameLabel.add_theme_font_override('font', load(name_label_font))
+  
+  %DialogicNode_NameLabel.add_theme_color_override("font_color", name_label_color)
+  
+  %DialogicNode_NameLabel.use_character_color = name_label_use_character_color
+  
+  %NameLabelPanel.self_modulate = name_label_box_modulate
