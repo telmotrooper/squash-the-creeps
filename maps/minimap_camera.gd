056c73f71e7367ec2e3fa6517b7b2166ec134373
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-06 17:45:43 -0300

    Refactored 'minimap_camera.gd'

--- maps/minimap_camera.gd
+++ maps/minimap_camera.gd
@@ -1,11 +1,14 @@
 extends Camera3D
 
+var texture_resolution = Vector2i(1000, 1000)
+
 func _ready() -> void:
   if current:
-    $Timer.start()
+    get_window().mode = Window.MODE_WINDOWED
+    DisplayServer.window_set_size(texture_resolution)
+    await get_tree().create_timer(0.2).timeout
 
-func _on_timer_timeout() -> void:
-  var texture = get_viewport().get_texture().get_image()
-  texture.save_png("test.png")
-  print("texture saved at resolution ", get_viewport().get_visible_rect().size)
-  get_tree().quit()
+    var texture = get_viewport().get_texture().get_image()
+    texture.save_png("map_texture.png")
+    print("minimap texture saved at resolution ", get_viewport().get_visible_rect().size)
+    get_tree().quit()
