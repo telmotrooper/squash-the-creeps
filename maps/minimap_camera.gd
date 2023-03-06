bb30bc6615ca82343f39e14b35d5986265619d5e
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-06 17:49:29 -0300

    Added 'save to' variable

--- maps/minimap_camera.gd
+++ maps/minimap_camera.gd
@@ -1,6 +1,7 @@
 extends Camera3D
 
 var texture_resolution = Vector2i(1000, 1000)
+@export var save_to: String = "test.png"
 
 func _ready() -> void:
   if current:
@@ -9,6 +10,6 @@ func _ready() -> void:
     await get_tree().create_timer(0.2).timeout
 
     var texture = get_viewport().get_texture().get_image()
-    texture.save_png("map_texture.png")
+    texture.save_png(save_to)
     print("minimap texture saved at resolution ", get_viewport().get_visible_rect().size)
     get_tree().quit()
