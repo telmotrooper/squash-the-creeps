4457bed0f1847fa12db4c7df77cf2d2a3fd037e5
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-08 12:11:34 -0300

    Checkpoint animation is much quicker

--- props/checkpoint_flag/flag.gd
+++ props/checkpoint_flag/flag.gd
@@ -1,8 +1,6 @@
 extends Node3D
 
-@export var material_1: Material
-@export var material_2: Material
-
+var played:= false
 var text_index = 1
 var text = "CHECKPOINT"
 var initial_flag_y: float
@@ -23,11 +21,13 @@ func _on_text_timer_timeout() -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
   # Lower flag the first time (when material is not overriden yet).
-  if not is_instance_valid($Flag.get_surface_override_material(0)):
+  if not played:
+    played = true
     var tween = create_tween()
+    tween.set_parallel(true)
     tween.tween_property($Flag, "position:y", -4, 1)
-    tween.tween_callback($Flag.set_surface_override_material.bind(0, material_2))
-    tween.tween_property($Flag, "position:y", initial_flag_y, 1)
+    tween.tween_property($Flag2, "position:y", initial_flag_y, 1)
+    tween.set_parallel(false)
     tween.tween_callback($TextTimer.start)
     tween.tween_callback($AudioStreamPlayer3D.play)
   else: # Otherwise just show the label.
