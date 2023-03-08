864b7d5605828fa6b5b32a5efb16ee3d04f9d324
Telmo "Trooper <telmo.trooper@gmail.com>
2023-03-07 22:41:34 -0300

    Enemies react faster, can follow paths in reverse and have a "surprised" animation when they notice the player (#171)
    * Re-parented CSGBox3D node
    
    * Adding blue enemy to sand platform
    
    * Working on enemy that traces back path
    
    * Renamed variable
    
    * Refactoring script
    
    * Refactoring script
    
    * Added two enemies to test map
    
    * Decreased time to response for blue enemy from 1 sec to 500 ms
    
    * Enemies collide with each other
    
    * Increase range where enemies can see you
    
    * Enemy now has animation when they detect the player
    
    ---------
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- entities/enemy/blue_enemy/blue_enemy.gd
+++ entities/enemy/blue_enemy/blue_enemy.gd
@@ -3,28 +3,42 @@ class_name BlueEnemy
 
 @export var patrolling_speed := 9
 @export var chasing_speed := 13
-enum {
-  PATROLLING,
-  ALERT,
-  CHASING
-}
+
+enum { PATROLLING, ALERT, CHASING }
+enum { GOING, RETURNING }
 
 var state = PATROLLING
+var path_state = GOING
 
 func _ready() -> void:
   if get_parent() is PathFollow3D:
-    # This will make the enemy look to the correct direction along the path.
+    # Make the enemy look to the correct direction along the path.
     get_parent().set_rotation_mode(4)
 
 func _physics_process(delta: float) -> void:
   super._physics_process(delta)
   
+  if get_parent() is PathFollow3D and not get_parent().loop:
+    # If path follow doesn't loop, go in reverse.
+    var progress_ratio = get_parent().progress_ratio
+    
+    if progress_ratio <= 0 and path_state == RETURNING:
+      path_state = GOING
+      rotation_degrees.y += 180
+    elif progress_ratio >= 1 and path_state == GOING:
+      path_state = RETURNING
+      rotation_degrees.y -= 180
+  
   match state:
     PATROLLING:
       if get_parent() is PathFollow3D:
-        get_parent().progress += patrolling_speed * delta
+        if path_state == GOING:
+          get_parent().progress += patrolling_speed * delta
+        elif path_state == RETURNING:
+          get_parent().progress -= patrolling_speed * delta
     ALERT:
       $ExclamationMark.show()
+      $AnimationPlayer.play("surprised")
       $AlertTimer.start()
       set_physics_process(false)
     CHASING:
@@ -48,5 +62,6 @@ func squash() -> void:
 
 func _on_AlertTimer_timeout() -> void:
   $ExclamationMark.hide()
+  $AnimationPlayer.play("float")
   state = CHASING
   set_physics_process(true)
