16ea240a534920cabd99c9ada8d8575c390dbb2a
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-08 21:03:42 -0300

    Made tree shake when hit

--- maps/extras/palm_tree/palm_tree.gd
+++ maps/extras/palm_tree/palm_tree.gd
@@ -5,4 +5,10 @@ var shaking := false
 func interact_on_spin() -> void:
   if not shaking:
     shaking = true
-    print("interact_on_spin")
+    var tween = create_tween().set_loops(2)
+    tween.tween_property($Pivot, "rotation_degrees:x", 2.5, 0.2)
+    tween.tween_property($Pivot, "rotation_degrees:x", 0, 0.2)
+    tween.tween_callback(done)
+
+func done() -> void:
+  shaking = false
