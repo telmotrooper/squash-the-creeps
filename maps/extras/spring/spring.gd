6b5abfacc3b9bb861cf413dba36e9dbdcbc758b0
Telmo "Trooper <telmo.trooper@gmail.com>
2023-03-11 01:57:14 -0300

    Spring (#178)
    * Working on spring
    
    * Basic spring behavior implemented
    
    * Spring returns player to its normal state when done and resets
    
    * Refactoring: added 'reset' function to 'spring.gd'
    
    * Fixed player with 'x' in title screen
    
    * Moved 'spring.gd' to 'maps/extras/spring'
    
    * Added custom mesh for spring
    
    ---------
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

---
+++ maps/extras/spring/spring.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+@export var speed := 35
+var running := false
+var listening := true
+var player_parent: Node
+var player: Node3D
+var fall_acceleration: float
+
+
+func _ready() -> void:
+  set_physics_process(false)
+
+func _physics_process(delta: float) -> void:
+  $SpringPath/PathFollow3D.progress += speed * delta
+  if $SpringPath/PathFollow3D.progress_ratio >= 1:
+    reset()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+  if listening:
+    listening = false
+    player = body
+    # Backup player parent and fall acceleration.
+    player_parent = player.get_parent()
+    fall_acceleration = player.fall_acceleration
+    player.fall_acceleration = 0
+    player.reparent($SpringPath/PathFollow3D)
+    set_physics_process(true)
+
+func reset() -> void:
+  player.fall_acceleration = fall_acceleration
+  player.reparent(player_parent)
+  $SpringPath/PathFollow3D.progress_ratio = 0
+  listening = true
+  set_physics_process(false)
