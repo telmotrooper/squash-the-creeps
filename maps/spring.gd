2e66a2ce3c3e217b99ae73341a3691c0ce3b67e0
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-11 00:15:36 -0300

    Basic spring behavior implemented

--- maps/spring.gd
+++ maps/spring.gd
@@ -1,14 +1,17 @@
 extends CSGBox3D
 
-@export var speed := 9
+@export var speed := 30
 var running := false
+var player: Node3D
 
 func _ready() -> void:
   set_physics_process(false)
 
 func _physics_process(delta: float) -> void:
+  player.fall_acceleration = 0
   $SpringPath/PathFollow3D.progress += speed * delta
 
-func _on_area_3d_body_entered(player: Node3D) -> void:
+func _on_area_3d_body_entered(body: Node3D) -> void:
+  player = body
   player.reparent($SpringPath/PathFollow3D)
   set_physics_process(true)
