e7f964207985954166f032f024def280f754bef3
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-11 01:10:22 -0300

    Refactoring: added 'reset' function to 'spring.gd'

--- maps/spring.gd
+++ maps/spring.gd
@@ -1,6 +1,6 @@
 extends CSGBox3D
 
-@export var speed := 30
+@export var speed := 35
 var running := false
 var listening := true
 var player_parent: Node
@@ -12,20 +12,24 @@ func _ready() -> void:
   set_physics_process(false)
 
 func _physics_process(delta: float) -> void:
-  player.fall_acceleration = 0
   $SpringPath/PathFollow3D.progress += speed * delta
   if $SpringPath/PathFollow3D.progress_ratio >= 1:
-    player.reparent(player_parent)
-    $SpringPath/PathFollow3D.progress_ratio = 0
-    player.fall_acceleration = fall_acceleration
-    listening = true
-    set_physics_process(false)
+    reset()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
   if listening:
     listening = false
     player = body
-    fall_acceleration = player.fall_acceleration
+    # Backup player parent and fall acceleration.
     player_parent = player.get_parent()
+    fall_acceleration = player.fall_acceleration
+    player.fall_acceleration = 0
     player.reparent($SpringPath/PathFollow3D)
     set_physics_process(true)
+
+func reset() -> void:
+  player.fall_acceleration = fall_acceleration
+  player.reparent(player_parent)
+  $SpringPath/PathFollow3D.progress_ratio = 0
+  listening = true
+  set_physics_process(false)
