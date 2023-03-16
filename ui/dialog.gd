db51f611c681c8e034a3c4cad5b76b21590f1328
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-16 00:33:31 -0300

    Write text based on time

--- ui/dialog.gd
+++ ui/dialog.gd
@@ -3,15 +3,19 @@ class_name Dialog
 
 signal finished
 
+var text_to_write := ""
+var index = 0
+
 func _ready() -> void:
   set_process(false)
+  %DialogText.text = ""
 
 func _process(_delta: float) -> void:
   if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("skip_dialog"):
     close_dialog()
 
 func set_text(text) -> void:
-  %DialogText.text = text
+  text_to_write = text
 
 func open_dialog() -> void:
   GameState.Player.paused = true
@@ -20,8 +24,17 @@ func open_dialog() -> void:
   var tween = create_tween()
   tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 0.5)
   tween.tween_callback(func(): set_process(true))
+  tween.tween_callback(func():
+    while index <= text_to_write.length():
+      await get_tree().create_timer(0.05).timeout
+      %DialogText.text = text_to_write.substr(0, index)
+      index += 1
+  )
 
 func close_dialog() -> void:
+  index = 0
+  %DialogText.text = ""
+  
   set_process(false)
   var tween = create_tween()
   tween.tween_property(self, "modulate", Color(1, 1, 1, 0), 0.5)
