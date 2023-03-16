593f7d2f3b7f5e8562c7b89eea65b736322540df
Telmo "Trooper <telmo.trooper@gmail.com>
2023-03-16 00:43:30 -0300

    Text is written character by character, interacting again skips to the end (#190)
    * Write text based on time
    
    * Moved clean up to tween callback
    
    * Clean up 'text_to_write'
    
    * Allow skipping dialog to the end
    
    ---------
    
    Co-authored-by: Telmo Valverde <telmo.valverde@tarmac.io>

--- ui/dialog.gd
+++ ui/dialog.gd
@@ -3,15 +3,23 @@ class_name Dialog
 
 signal finished
 
+var text_to_write := ""
+var is_writing := false
+var index = 0
+
 func _ready() -> void:
   set_process(false)
+  %DialogText.text = ""
 
 func _process(_delta: float) -> void:
   if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("skip_dialog"):
-    close_dialog()
+    if is_writing:
+      index = text_to_write.length()
+    else:
+      close_dialog()
 
 func set_text(text) -> void:
-  %DialogText.text = text
+  text_to_write = text
 
 func open_dialog() -> void:
   GameState.Player.paused = true
@@ -20,12 +28,23 @@ func open_dialog() -> void:
   var tween = create_tween()
   tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 0.5)
   tween.tween_callback(func(): set_process(true))
+  tween.tween_callback(func():
+    is_writing = true
+    while index <= text_to_write.length():
+      await get_tree().create_timer(0.05).timeout
+      %DialogText.text = text_to_write.substr(0, index)
+      index += 1
+    is_writing = false
+  )
 
-func close_dialog() -> void:
+func close_dialog() -> void:  
   set_process(false)
   var tween = create_tween()
   tween.tween_property(self, "modulate", Color(1, 1, 1, 0), 0.5)
   tween.tween_callback(func():
+    index = 0
+    text_to_write = ""
+    %DialogText.text = ""
     hide()
     GameState.minimap.show()
     GameState.Player.paused = false
