557a039c3c224b74650a9fb64021684ff7eda39e
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-18 13:35:44 -0300

    Refactoring: simplified scene transition even more

--- main.gd
+++ main.gd
@@ -2,9 +2,6 @@ extends Node3D
 
 @export var initial_scene: PackedScene
 
-enum { LOADING, LOADED, PLAYING }
-
-var state := PLAYING
 var current_scene = null
 var loading_scene = null
 
@@ -15,32 +12,26 @@ func _ready() -> void:
 
 func load_scene(scene_to_load: NodePath) -> void:
   loading_scene = scene_to_load
-  ResourceQueue.queue_resource(loading_scene)
-  
-  state = LOADING
-  $FadeTransition.fade_out()
   
   if not ResourceLoader.has_cached(loading_scene):
     $ProgressBar.value = 0
     $ProgressBar.show()
-#    while not ResourceQueue.is_ready(loading_scene):
-#      $ProgressBar.value = ResourceQueue.get_progress(loading_scene) * 100
-
-func _on_fade_transition_faded_out() -> void:
-  if state == LOADING:
-    $WorldScene.hide()
-    
-    if current_scene:
-      $WorldScene.remove_child(current_scene)
-      if is_instance_valid(current_scene):
-        current_scene.queue_free()
-      current_scene = null
-    set_process(true)
-
-func _on_fade_transition_faded_in() -> void:
-  state = PLAYING
+  
+  ResourceQueue.queue_resource(loading_scene)
+  
+  $FadeTransition.fade_out()
+  await $FadeTransition.faded_out
+  
+  if current_scene:
+    $WorldScene.remove_child(current_scene)
+    if is_instance_valid(current_scene):
+      current_scene.queue_free()
+    current_scene = null
+  set_process(true)
 
 func _process(_delta: float) -> void:
+  #$ProgressBar.value = ResourceQueue.get_progress(loading_scene) * 100  
+  
   # Waiting until loading is done.
   var new_world = ResourceQueue.get_resource(loading_scene)
   
@@ -49,12 +40,11 @@ func _process(_delta: float) -> void:
     current_scene = new_world.instantiate()
     
     $WorldScene.add_child(current_scene)
-    $WorldScene.show()
     
     $ProgressBar.value = 100
     $ProgressBar.hide()
     
-    state = LOADED
     $FadeTransition.fade_in()
     
     set_process(false)
+    await $FadeTransition.faded_in
