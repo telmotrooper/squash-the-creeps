a0a7ee27c647c4cf41413be269eab60749712096
Telmo Valverde <telmo.valverde@tarmac.io>
2023-03-19 20:55:10 -0300

    Allow skipping portal cutscene

--- ui/skip_cutscene.gd
+++ ui/skip_cutscene.gd
@@ -1,13 +1,19 @@
 extends Control
+class_name SkipCutscene
 
 @export var animation_player: AnimationPlayer
 
 var first_time := true
+signal skip
+
+enum Mode { ANIMATION_PLAYER, SIGNAL }
+var mode := Mode.ANIMATION_PLAYER
 
 func _ready() -> void:
   hide()
-  animation_player.connect("animation_started", enable)
-  animation_player.connect("animation_finished", disable)
+  if animation_player:
+    animation_player.connect("animation_started", enable)
+    animation_player.connect("animation_finished", disable)
   set_process(false)
 
 func _process(_delta: float) -> void:
@@ -16,7 +22,10 @@ func _process(_delta: float) -> void:
       show()
       first_time = false
     else:
-      animation_player.play("RESET") # Skip cutscene.
+      if mode == Mode.ANIMATION_PLAYER:
+        animation_player.play("RESET") # Skip cutscene.
+      else:
+        emit_signal("skip")
       first_time = true # Reset counter for future cutscenes.
       hide()
       set_process(false)
@@ -27,3 +36,6 @@ func enable(_anim_name: StringName) -> void:
 func disable(_anim_name: StringName) -> void:
   hide()
   set_process(false)
+
+func set_mode(new_mode: Mode) -> void:
+  mode = new_mode
