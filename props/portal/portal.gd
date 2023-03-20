a17f18fa408ab65526d943c5daac0decc0febd64
Telmo "Trooper <telmo.trooper@gmail.com>
2023-03-19 21:16:49 -0300

    Show "health replenished" when entering already active checkpoint, allow skipping portal unlock cutscenes (#198)
    * Changed checkpoint message when it's already active
    
    * Allow skipping portal cutscene
    
    * Made arg optional in 'enable' and 'disable'
    
    * Replaced integers with enum values
    
    * Fixed 'fade_out' animation in portal when player skips cutscene
    
    * Moved a bit of logic to 'finish_cutscene'

--- props/portal/portal.gd
+++ props/portal/portal.gd
@@ -3,9 +3,13 @@ extends CSGCylinder3D
 @export var label: String
 @export var map_name: String
 @export var godot_heads_required: int
+@export var skip_cutscene: SkipCutscene
 
 func _ready() -> void:
   $Label3D.text = label if label else map_name
+  
+  if skip_cutscene:
+    skip_cutscene.connect("skip", finish_cutscene)
   
   if godot_heads_required >= 1 and portal_locked():
     $RequirementLabel.text = "Godot Heads x %d" % godot_heads_required
@@ -17,13 +21,14 @@ func _ready() -> void:
   
     if is_instance_valid(child_camera): # Cutscene
       child_camera.make_current()
-      var timer = get_tree().create_timer(1, false)
-      await timer.timeout
+      skip_cutscene.set_mode(SkipCutscene.Mode.SIGNAL)
+      skip_cutscene.enable()
+      await get_tree().create_timer(1, false).timeout
       $RequirementAnimationPlayer.play("fade_out")
-      timer = get_tree().create_timer(2, false)
-      await timer.timeout
-      GameState.Player.get_node("%Camera3D").make_current()
-      GameState.portal_unlocked[get_path()] = true
+      await get_tree().create_timer(2, false).timeout
+      finish_cutscene()
+      skip_cutscene.disable()
+      skip_cutscene.set_mode(SkipCutscene.Mode.ANIMATION_PLAYER)
 
 func _on_Portal_entered(_body: Node) -> void:
   GameState.hub_1_at_night = false
@@ -45,3 +50,7 @@ func requirement_met() -> bool:
 
 func portal_locked() -> bool:
   return not GameState.portal_unlocked.has(get_path())
+
+func finish_cutscene() -> void:
+  GameState.portal_unlocked[get_path()] = true
+  GameState.Player.get_node("%Camera3D").make_current()
