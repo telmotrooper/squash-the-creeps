ad0faabca11b8cd57c1f1eb7b6ccf72a74f59db9
Telmo Valverde <telmo.valverde@tarmac.io>
2023-07-10 22:41:30 -0300

    Tiki is happy when saved

--- maps/extras/caged_tiki_npc/caged_tiki_npc.gd
+++ maps/extras/caged_tiki_npc/caged_tiki_npc.gd
@@ -9,3 +9,7 @@ func free_tiki() -> void:
   await get_tree().create_timer(1).timeout
   $tiki_npc/Label3D.text = "THANKS!"
   $tiki_npc/Label3D.show()
+  var tween_2 = create_tween().set_loops()
+  tween_2.tween_property($tiki_npc, "position:y", 0.5, 0.25)
+  tween_2.tween_property($tiki_npc, "position:y", 0, 0.25)
+  
