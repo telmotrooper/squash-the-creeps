4a9d59c30e945f3bb9f6de5a7082931d8abd32b8
Telmo Valverde <telmo.valverde@tarmac.io>
2023-07-11 19:40:32 -0300

    Allow shaking spaceship

--- props/spaceship/spaceship_hub.gd
+++ props/spaceship/spaceship_hub.gd
@@ -1,7 +1,11 @@
 extends StaticBody3D
 
+var shaking := false
+var initial_rotation: Vector3
+
 func _ready() -> void:
   set_process(false)
+  initial_rotation = self.rotation_degrees
 
 func _process(_delta: float) -> void:
   if Input.is_action_just_pressed("interact") and not GameState.Player.paused:
@@ -15,3 +19,14 @@ func _on_SpaceshipArea_body_entered(_player: Node) -> void:
 func _on_SpaceshipArea_body_exited(_player: Node) -> void:
   %SpaceshipLabel3D.hide()
   set_process(false)
+
+func interact_on_spin() -> void:
+  if not shaking:
+    shaking = true
+    var tween = create_tween().set_loops(2)
+    tween.tween_property(self, "rotation_degrees:z", initial_rotation.z + 2.5, 0.2)
+    tween.tween_property(self, "rotation_degrees:z", initial_rotation.z, 0.2)
+    tween.tween_callback(done_shaking)
+
+func done_shaking() -> void:
+  shaking = false
