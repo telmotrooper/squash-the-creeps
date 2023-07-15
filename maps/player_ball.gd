b0a3a3ed0835c2ac075d2cae85eda98ed872368f
Telmo Valverde <telmo.valverde@tarmac.io>
2023-07-14 23:12:24 -0300

    Player ball movement considers camera position

--- maps/player_ball.gd
+++ maps/player_ball.gd
@@ -11,16 +11,23 @@ func _ready() -> void:
   initial_position = global_transform.origin
 
 func _physics_process(delta: float) -> void:
+  # Make camera follow player ball.
   %CameraPivot.position = initial_position + position
-  if Input.is_action_pressed("move_forward"):
-    angular_velocity.x -= force * delta
-  elif Input.is_action_pressed("move_back"):
-    angular_velocity.x += force * delta
   
-  if Input.is_action_pressed("move_left"):
-    angular_velocity.z += force * delta
-  elif Input.is_action_pressed("move_right"):
-    angular_velocity.z -= force * delta
+  # Get direction vector based checked input.
+  var direction = Vector3(
+    Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+    0,
+    Input.get_action_strength("move_back") - Input.get_action_strength("move_forward"))
+  
+  if direction != Vector3.ZERO:
+    # Rotate direction based checked camera.
+    var horizontal_rotation = %CameraPivot/Horizontal.global_transform.basis.get_euler().y
+    direction = direction.rotated(Vector3.UP, horizontal_rotation).normalized()
+    # Move player ball according to input and camera direction.
+    angular_velocity.x += direction.z * force * delta
+    angular_velocity.z -= direction.x * force * delta
+
 
 func move_to_last_safe_position() -> void:
   position = Vector3.ZERO # Start of the map.
