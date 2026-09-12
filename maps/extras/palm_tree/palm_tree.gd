3d45e3a7fc7a1193f03bbc4a731782e1e7f88188
Telmo Valverde <telmo.trooper@gmail.com>
2026-09-12 18:04:23 -0300

    tilt tree based on player position

--- maps/extras/palm_tree/palm_tree.gd
+++ maps/extras/palm_tree/palm_tree.gd
@@ -1,6 +1,8 @@
 extends StaticBody3D
 
 const tilt_degrees := 2.5
+const tilt_duration := 0.2 # seconds
+
 var shaking := false
 
 func _ready() -> void:
@@ -8,7 +10,7 @@ func _ready() -> void:
 		if child is Gem:
 			child.freeze = true
 
-func interact_on_spin(_player_position: Vector3) -> void:
+func interact_on_spin(player_position: Vector3) -> void:
 	if shaking:
 		return
 	
@@ -17,10 +19,19 @@ func interact_on_spin(_player_position: Vector3) -> void:
 	for child in get_children(): # Drop gem.
 		if child is Gem:
 			child.freeze = false
-
-	var tween = create_tween().set_loops(2)
-	tween.tween_property($Pivot, "rotation_degrees:x", tilt_degrees, 0.2)
-	tween.tween_property($Pivot, "rotation_degrees:x", 0, 0.2)
+	
+	var to_player := to_local(player_position)
+	
+	# Horizontal axis perpendicular to the hit direction.
+	var axis := to_player.cross(Vector3.UP).normalized()
+	
+	var tween := create_tween().set_loops(2)
+	tween.tween_method(tilt.bind(axis), 0.0, tilt_degrees, tilt_duration)
+	tween.tween_method(tilt.bind(axis), tilt_degrees, 0.0, tilt_duration)
+	
 	await tween.finished
 	
 	shaking = false
+
+func tilt(angle_degrees: float, axis: Vector3) -> void:
+	$Pivot.basis = Basis(axis, deg_to_rad(angle_degrees))
