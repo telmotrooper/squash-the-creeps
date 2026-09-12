b9a89cd8d1806888dc4643ab0e36b9973259b39d
Telmo Valverde <telmo.trooper@gmail.com>
2026-09-12 16:06:43 -0300

    fix not being able to shake trees non-stop

--- maps/extras/palm_tree/palm_tree.gd
+++ maps/extras/palm_tree/palm_tree.gd
@@ -10,15 +10,15 @@ func _ready() -> void:
 
 func interact_on_spin() -> void:
 	if not shaking:
+		shaking = true
+		
 		for child in get_children(): # Drop gem.
 			if child is Gem:
 				child.freeze = false
 	
-	shaking = true
-	var tween = create_tween().set_loops(2)
-	tween.tween_property($Pivot, "rotation_degrees:x", tilt_degrees, 0.2)
-	tween.tween_property($Pivot, "rotation_degrees:x", 0, 0.2)
-	tween.tween_callback(done)
-
-func done() -> void:
-	shaking = false
+		var tween = create_tween().set_loops(2)
+		tween.tween_property($Pivot, "rotation_degrees:x", tilt_degrees, 0.2)
+		tween.tween_property($Pivot, "rotation_degrees:x", 0, 0.2)
+		await tween.finished
+		
+		shaking = false
