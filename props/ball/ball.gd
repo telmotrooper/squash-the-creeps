243836faead667764cbf4a29411c2c01e5134fca
Telmo Valverde <telmo.trooper@gmail.com>
2026-09-13 20:21:57 -0300

    player can spin the ball

--- props/ball/ball.gd
+++ props/ball/ball.gd
@@ -1,4 +1,22 @@
 extends RigidBody3D
 
+var just_spinned := false
+
+var throw_speed := 1.0
+var throw_lift := 0.0
+var tumble_torque := 1.0
+
 func interact_on_spin(player_position: Vector3) -> void:
+	if just_spinned:
+		return
+	just_spinned = true
 	print("player spins ball")
+	
+	var direction := global_position - player_position
+	direction.y = 0.0
+	
+	apply_central_impulse(direction * throw_speed * Vector3.UP * throw_lift)
+	apply_torque_impulse(Vector3.UP.cross(direction) * tumble_torque)
+	
+	await get_tree().create_timer(0.5).timeout
+	just_spinned = false
