73f62e0568e77ff3a574e0013dea66bff204b295
Telmo Valverde <telmo.trooper@gmail.com>
2026-09-13 23:14:53 -0300

    hitting the spaceship shakes it in the direction the player is facing

--- props/spaceship/spaceship_hub.gd
+++ props/spaceship/spaceship_hub.gd
@@ -1,11 +1,14 @@
 extends StaticBody3D
 
+const tilt_degrees := 2.5
+const tilt_duration := 0.2 # seconds
+
 var shaking := false
-var initial_rotation: Vector3
+var initial_basis: Basis
 
 func _ready() -> void:
 	set_process(false)
-	initial_rotation = self.rotation_degrees
+	initial_basis = basis
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and not GameState.player.paused:
@@ -20,13 +23,24 @@ func _on_SpaceshipArea_body_exited(_player: Node) -> void:
 	%SpaceshipLabel3D.hide()
 	set_process(false)
 
-func interact_on_spin(_player_position: Vector3) -> void:
-	if not shaking:
-		shaking = true
-		var tween = create_tween().set_loops(2)
-		tween.tween_property(self, "rotation_degrees:z", initial_rotation.z + 2.5, 0.2)
-		tween.tween_property(self, "rotation_degrees:z", initial_rotation.z, 0.2)
-		tween.tween_callback(done_shaking)
-
-func done_shaking() -> void:
+func interact_on_spin(player_position: Vector3) -> void:
+	if shaking:
+		return
+	
+	shaking = true
+	
+	var to_player := to_local(player_position)
+	
+	# Horizontal axis perpendicular to the hit direction.
+	var axis := to_player.cross(Vector3.UP).normalized()
+	
+	var tween := create_tween().set_loops(2)
+	tween.tween_method(tilt.bind(axis), 0.0, tilt_degrees, tilt_duration)
+	tween.tween_method(tilt.bind(axis), tilt_degrees, 0.0, tilt_duration)
+	
+	await tween.finished
+	
 	shaking = false
+
+func tilt(angle_degrees: float, axis: Vector3) -> void:
+	basis = initial_basis * Basis(axis, deg_to_rad(angle_degrees))
