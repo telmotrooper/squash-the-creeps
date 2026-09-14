0b470bc9252bc99c8e6cd147d71ae968c077f8f0
Telmo Valverde <telmo.trooper@gmail.com>
2026-09-13 21:00:15 -0300

    fix central impulse and rename variables

--- props/ball/ball.gd
+++ props/ball/ball.gd
@@ -2,9 +2,9 @@ extends RigidBody3D
 
 var just_spun := false
 
-var throw_speed := 1.0
-var throw_lift := 0.0
-var tumble_torque := 1.0
+var throw_impulse := 1.0
+var lift_impulse := 2.0
+var rotational_impulse := 1.0
 
 func interact_on_spin(player_position: Vector3) -> void:
 	if just_spun:
@@ -13,9 +13,10 @@ func interact_on_spin(player_position: Vector3) -> void:
 	
 	var direction := global_position - player_position
 	direction.y = 0.0
+	direction = direction.normalized()
 	
-	apply_central_impulse(direction * throw_speed * Vector3.UP * throw_lift)
-	apply_torque_impulse(Vector3.UP.cross(direction) * tumble_torque)
+	apply_central_impulse(direction * throw_impulse + Vector3.UP * lift_impulse)
+	apply_torque_impulse(Vector3.UP.cross(direction) * rotational_impulse)
 	
 	await get_tree().create_timer(0.5).timeout
 	just_spun = false
